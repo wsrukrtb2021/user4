4681b08d55e5a2e19aeba71c846f2a6a1ee33474
unknown <lorp.lop@yandex.ru>
2021-06-01 17:07:59 +0300

    manan

---
+++ Big pack/Big pack/Class/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Big_pack.Class
+{
+    class conecnt
+    {
+         public static string String { get; } = "Data Source = WSR; Intial Catalog = usercontrol;user id = user4; password = passuser4";
+    }
+}
